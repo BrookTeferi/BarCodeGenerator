9423fcf15a76b40d112c49dee4e9a392bceaec40
brookteferi1q2w <brookteferi1q2w@gmail.com>
2023-04-20 18:12:14 +0300

    Add project files.

---
+++ BarCodeGenerator/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using BarCodeGenerator.Models;
+using IronBarCode;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace BarCodeGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public IActionResult CreateBarcode()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateBarcode(GenerateBarcodeModel generateBarcode)
+        {
+            try
+            {
+                GeneratedBarcode barcode = IronBarCode.BarcodeWriter.CreateBarcode(generateBarcode.BarcodeText, BarcodeWriterEncoding.Code128);
+                barcode.ResizeTo(400, 120);
+                barcode.AddBarcodeValueTextBelowBarcode();
+                barcode.ChangeBarCodeColor(Color.Black);
+                barcode.SetMargins(10);
+                string path = Path.Combine(_environment.WebRootPath, "GeneratedBarcode");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                string filePath = Path.Combine(_environment.WebRootPath, "GeneratedBarcode/barcode.png");
+                barcode.SaveAsPng(filePath);
+                string fileName = Path.GetFileName(filePath);
+                string imageUrl = $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}" + "/GeneratedBarcode/" + fileName;
+                ViewBag.QrCodeUri = imageUrl;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return View();
+        }
+
+    }
+}
---
+++ BarCodeGenerator/Models/GenerateBarcodeModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BarCodeGenerator.Models
+{
+    public class GenerateBarcodeModel
+    {
+        [Display(Name = "Enter Barcode Text")]
+        public string BarcodeText
+        {
+            get;
+            set;
+        }
+    }
+}
